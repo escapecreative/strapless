82d1e1565e0477c57470e8688b2c35ef8048a6ff
Dave Warfel <dave@escapecreative.com>
2013-09-21 22:52:02 -0400

    Remove wp.me shortlink from <head>

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 // Add RSS links to <head> section
 automatic_feed_links();
